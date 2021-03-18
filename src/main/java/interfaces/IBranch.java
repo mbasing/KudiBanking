5a2c93a242e4a973b3c5c875a5c3320bd70410cc
Moses Basing <mosesbasing@gmail.com>
2021-03-18 15:24:20 +0000

    Implement Branch interface

--- src/main/java/interfaces/IBranch.java
+++ src/main/java/interfaces/IBranch.java
@@ -1,4 +1,12 @@
 package interfaces;
 
+import classes.Customer;
+
+import java.util.ArrayList;
+
 public interface IBranch {
+    String getName();
+    ArrayList<Customer> getCustomers();
+    boolean newCustomer(String nameOfCustomer, double initialTransaction);
+    boolean addCustomerTransaction(String nameOfCustomer, double transaction);
 }
