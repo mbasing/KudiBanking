3f9ae9787609cfd2b0115ced9326a5cabf83d34d
Moses Basing <mosesbasing@gmail.com>
2021-03-18 15:29:27 +0000

    Implement Bank interface

--- src/main/java/interfaces/IBank.java
+++ src/main/java/interfaces/IBank.java
@@ -1,4 +1,10 @@
 package interfaces;
 
 public interface IBank {
+
+    boolean addBranch(String nameOfBranch);
+    boolean addCustomer(String nameOfBranch, String nameOfCustomer, double initialTransaction);
+    boolean addCustomerTransaction(String branchName, String customerName, double transaction);
+    boolean listCustomers(String nameOfBranch, boolean printTransactions);
 }
+
