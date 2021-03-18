719b82f71de0fff9f7da3d6745f3329aa0950d36
Moses Basing <mosesbasing@gmail.com>
2021-03-18 13:28:45 +0000

    Implement Customer class

--- src/main/java/classes/Customer.java
+++ src/main/java/classes/Customer.java
@@ -1,4 +1,34 @@
 package classes;
 
-public class Customer {
+import interfaces.ICustomer;
+
+import java.util.ArrayList;
+
+public class Customer implements ICustomer {
+
+    private String name;
+    private ArrayList<Double> transactions;
+
+    public Customer(String nameOfCustomer, double initialTransaction){
+        this.name = nameOfCustomer;
+        this.transactions = new ArrayList<Double>();
+        this.transactions.add(initialTransaction);
+
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public ArrayList<Double> getTransactions() {
+        return this.transactions;
+    }
+
+    @Override
+    public void addTransaction(double transaction) {
+        this.transactions.add(transaction);
+    }
+
 }
