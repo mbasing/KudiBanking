d89a9e0c9a7893de5790dc794a2df9e81262de24
Moses Basing <mosesbasing@gmail.com>
2021-03-18 15:07:29 +0000

    Implement Customer interface

--- src/main/java/interfaces/ICustomer.java
+++ src/main/java/interfaces/ICustomer.java
@@ -1,4 +1,10 @@
 package interfaces;
 
+import java.util.ArrayList;
+
 public interface ICustomer {
+
+    String getName();
+    ArrayList<Double> getTransactions();
+    void addTransaction(double transaction);
 }
